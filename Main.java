11c603b4765fab5986d29e28e3e9e2198f39b445
Kamil <mrkamkam2053@gmail.com>
2024-05-04 12:08:28 +0200

    3

--- Main.java
+++ Main.java
@@ -2,18 +2,28 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.interfaces.Messenger;
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
+
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Person person = new Person("Patryk", -1);
+            Person person = new Person("Patryk", 7);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
         } catch (InvalidAgeException e) {
             System.out.println("Too young/old: " + e.getMessage());
         }
 
+        int result = MathUtils.add(10, 15);
+
         Messenger messenger = new EmailMessenger();
-        messenger.sendMessage("Hello");
+        messenger.sendMessage("The sum is equal: " + result);
+
+
+
+
+
+
     }
 }
--- company/implementations/EmailMessenger.java
+++ company/implementations/EmailMessenger.java
@@ -5,6 +5,6 @@ import company.interfaces.Messenger;
 public class EmailMessenger implements Messenger {
     @Override
     public void sendMessage(String message) {
-        System.out.println("Sending email: " + message);
+        System.out.println("Email is: " + message);
     }
 }
